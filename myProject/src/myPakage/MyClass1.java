396d0d057946ceec20f6b1e1fae4c622cf97d7c8
smztks0517 <>
2021-11-17 09:50:29 +0900

    no message

--- myProject/src/myPakage/MyClass1.java
+++ myProject/src/myPakage/MyClass1.java
@@ -5,6 +5,8 @@ public class MyClass1 {
 	public static void main(String[] args) {
 		System.out.println("Hell World");
 		System.out.println("Hell World");
+		System.out.println("Hell World");
+		System.out.println("Hell World");
 	}
 
 }
